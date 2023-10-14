f802a464bf404291181c79a2cc5fce52889bd7d9
n3trnnr <n3trnnr@gmail.com>
2023-10-14 22:12:09 +0300

    edited app.js

--- app.js
+++ app.js
@@ -26,80 +26,80 @@ function createTodo(id, isChecked, userName, title) {
 }
 
 const getData = async () => {
-    try{
+    try {
         const [usersResponse, todosResponse] = await Promise.all([
             fetch('https://jsonplaceholder.typicode.com/users'),
             fetch('https://jsonplaceholder.typicode.com/todos')
         ])
-    if(!usersResponse.ok){
-        throw new Error(`${usersResponse.status}`)
-    }else if(!todosResponse.ok){
-        throw new Error(`${todosResponse.status}`)
-    }else{
-        const users = await usersResponse.json()
-        const todos = await todosResponse.json()
-        // pagination(todos, users)
+        if (!usersResponse.ok) {
+            throw new Error(`${usersResponse.status}`)
+        } else if (!todosResponse.ok) {
+            throw new Error(`${todosResponse.status}`)
+        } else {
+            const users = await usersResponse.json()
+            const todos = await todosResponse.json()
 
-        usersArr.push(...users)
+            usersArr.push(...users)
 
-        users.forEach((user) => {
-            const option = document.createElement('option')
-            option.innerText = user.name
-            select.appendChild(option)
-       })
-    
-        users.forEach((user)=> {
-            todos.forEach((todo)=>{
-                if(user.id === todo.userId){
-                createTodo(todo.id, todo.completed, user.name, todo.title)
-            }
-        })})
-   }
+            users.forEach((user) => {
+                const option = document.createElement('option')
+                option.innerText = user.name
+                select.appendChild(option)
+            })
+
+            users.forEach((user) => {
+                todos.forEach((todo) => {
+                    if (user.id === todo.userId) {
+                        createTodo(todo.id, todo.completed, user.name, todo.title)
+                    }
+                })
+            })
+        }
     }
-    catch(error){
+    catch (error) {
         alert(error)
     }
 }
 
-async function postData(event){
+async function postData(event) {
     event.preventDefault()
     if (!input.value || select.value === 'select user') {
         alert('Заполните все необходимые поля!')
         input.value = ''
     } else {
-            const user = usersArr.find(user => user.name === select.value)
-            try{
-                const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        userId: user.id,
-                        id: todoList.childElementCount + 1,
-                        title: input.value,
-                        completed: false
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                if(!res.ok){
-                    throw new Error(`${res.status}`)
-                }else{
-                    const data = await res.json()
-                    if(Object.keys(data).length){
-                        console.log('POST', data);
-                        createTodo(data.id, data.completed, select.value, data.title)
-                    }
+        const user = usersArr.find(user => user.name === select.value)
+        try {
+            const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
+                method: 'POST',
+                body: JSON.stringify({
+                    userId: user.id,
+                    id: todoList.childElementCount + 1,
+                    title: input.value,
+                    completed: false
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
                 }
-            }catch(error){
-                alert(error)
+            })
+            if (!res.ok) {
+                throw new Error(`${res.status}`)
+            } else {
+                const data = await res.json()
+                if (Object.keys(data).length) {
+                    console.log('POST', data);
+                    createTodo(data.id, data.completed, select.value, data.title)
+                }
             }
+        } catch (error) {
+            alert(error)
+        }
         input.value = ''
     }
-} 
+}
 
-async function patchData(event){
+async function patchData(event) {
     if (event.target.tagName === 'INPUT') {
-        try{
+        try {
             const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${+event.target.parentElement.id}`, {
                 method: 'PATCH',
                 body: JSON.stringify({
@@ -109,10 +109,10 @@ async function patchData(event){
                     'Content-Type': 'application/json'
                 }
             })
-            if(!res.ok){
-                event.target.checked === true? event.target.checked = false : event.target.checked = true
+            if (!res.ok) {
+                event.target.checked === true ? event.target.checked = false : event.target.checked = true
                 throw new Error(`${res.status}`)
-            }else{
+            } else {
                 const data = await res.json()
                 console.log('PATCH: ', data);
                 const elements = todoList.children
@@ -122,56 +122,30 @@ async function patchData(event){
                     }
                 }
             }
-         }catch(error){
+        } catch (error) {
             alert(error)
         }
     }
 }
 
-async function deleteData(event){
+async function deleteData(event) {
     if (event.target.classList.contains('close')) {
-        try{
+        try {
             const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${+event.target.parentElement.id}`, {
                 method: 'DELETE'
             })
-            if(!res.ok){
+            if (!res.ok) {
                 throw new Error(`${res.status}`)
-            }else{
+            } else {
                 event.target.parentElement.remove()
                 const data = await res.json()
                 console.log('DELETE: ', data)
             }
-        }catch(error){
+        } catch (error) {
             alert(error)
-        } 
+        }
     }
 }
-
-// function pagination(todos, users){
-//     const todosArr = [...todos]
-//     const usersArr = [...users]
-//     const nav = document.querySelector('#btn-wrapper')
-
-//     let currentPage = 2
-//     let pages = [...Array(Math.ceil(todosArr.length / 30)).keys()]
-//     let start = (currentPage - 1) * 30 
-//     let end = currentPage * 30
-
-//     pages.forEach((page, index)=>{
-//         const btn = document.createElement('button')
-//         btn.innerText = index
-//         nav.append(btn)
-//     })
-
-//     const paginator = todosArr.slice(start, end)
-//     console.log(paginator);
-//     users.forEach((user)=> {
-//         paginator.forEach((todo)=>{
-//             if(user.id === todo.userId){
-//             createTodo(todo.id, todo.completed, user.name, todo.title)
-//         }
-//     })})
-// }
 
 btn.addEventListener('click', postData)
 todoList.addEventListener('click', patchData)
